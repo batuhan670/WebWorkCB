f1cc135140501c5da9543ee007cee54b713752f0
batuhan670 <113347562+batuhan670@users.noreply.github.com>
2023-01-17 21:24:29 +0100

    css

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import "./FormStyles.css"
 
 
 
@@ -10,27 +11,40 @@ const Forms = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_c6b2p35',
+      'template_r6g7c06',
+      form.current,
+      'e_uV2THf0e3Hl4jlx')
       .then((result) => {
         console.log(result.text);
+        console.log("message sent")
       }, (error) => {
         console.log(error.text);
       });
   };
-  return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
 
-    </div>
-  )
+
+}
+
+
+
+return (
+  <div>
+    <form ref={form} onSubmit={sendEmail} >
+      <label>Dein Name</label>
+      <label className='error'>  </label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <label className='error'>  </label>
+      <input type="email" name="user_email" />
+      <label>Nachricht</label>
+      <label className='error' >Bitte gib deine Nachricht ein </label>
+      <textarea name="nachricht" rows="6" placeholder='Tippe hier deine Nachricht ein' />
+      <input type="submit" value="Senden" className='btn' />
+    </form>
+
+  </div>
+)
 
 
 
