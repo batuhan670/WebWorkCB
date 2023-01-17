5e33805715875bddde710bab3a8b6f79abe604bb
batuhan670 <113347562+batuhan670@users.noreply.github.com>
2023-01-17 18:18:48 +0100

    gg

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,69 +1,29 @@
-import React from 'react'
-import { render } from '@react-email/render';
-import AWS from 'aws-sdk';
-import EmailContent from './EmailContent.js';
-import "./FormStyles.css"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
+export const ContactUs = () => {
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
-
-class Forms extends React.Component {
-  constructor(props) {
-    super(props);
-    this.sendEmail = this.sendEmail.bind(this);
-    this.input = React.createRef();
-  }
-
-  async sendEmail(event) {
-    alert('A name was submitted: ' + this.input.current.value);
-
-    AWS.config.update({region: 'eu-central-1'
-  });  
-    const emailHtml = render(<EmailContent text={this.input.current.value} url="https://example.com" />);
-  
-    const options = {
-      Source: 'Web.Work@outlook.at',
-      Destination: {
-        ToAddresses: ['batuhandanisment@hotmail.com'],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Charset: 'UTF-8',
-            Data: emailHtml,
-          },
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'hello world',
-        },
-      },
-    };
-  
-    const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' })
-      .sendEmail(options)
-      .promise();
-    const result = await sendPromise;
-    console.log(result);
-
-
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.sendEmail}>
-        <label>
-          Name:
-          <input type="text" ref={this.input} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-
-
-export default Forms
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
